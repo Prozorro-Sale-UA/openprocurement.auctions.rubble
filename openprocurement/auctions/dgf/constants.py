9fba6b0958c8bfa5d984c9db65e6d72eba4c1328
Andrew Leitsius <leits@quintagroup.com>
2018-02-21 19:11:23 +0200

    Set address required from 2020

--- openprocurement/auctions/dgf/constants.py
+++ openprocurement/auctions/dgf/constants.py
@@ -27,7 +27,7 @@ DGF_ID_REQUIRED_FROM = datetime(2017, 1, 1, tzinfo=TZ)
 DGF_DECISION_REQUIRED_FROM = datetime(2017, 1, 1, tzinfo=TZ)
 CLASSIFICATION_PRECISELY_FROM = datetime(2017, 7, 19, tzinfo=TZ)
 MINIMAL_EXPOSITION_REQUIRED_FROM = datetime(2017, 11, 17, tzinfo=TZ)
-DGF_ADDRESS_REQUIRED_FROM = datetime(2018, 2, 8, tzinfo=TZ)
+DGF_ADDRESS_REQUIRED_FROM = datetime(2020, 2, 8, tzinfo=TZ)
 
 #codes
 CAVPS_CODES = read_json('cav_ps.json')

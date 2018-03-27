05d8b6e69f72e78ad5a3d8d3b39ee81580191243
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-27 18:22:47 +0300

    Fix import

--- openprocurement/auctions/dgf/tests/blanks/tender_blanks.py
+++ openprocurement/auctions/dgf/tests/blanks/tender_blanks.py
@@ -8,7 +8,7 @@ from openprocurement.auctions.core.constants import (
     DGF_CDB2_CLASSIFICATION_PRECISELY_FROM as CLASSIFICATION_PRECISELY_FROM,
     DGF_CDB2_ADDRESS_REQUIRED_FROM as DGF_ADDRESS_REQUIRED_FROM
 )
-from openprocurement.auctions.core.models import SANDBOX_MODE
+from openprocurement.api.constants import SANDBOX_MODE
 from openprocurement.api.models import get_now, TZ
 from openprocurement.auctions.dgf.tests.base import (
     test_auction_maximum_data,

0fc64a3112713e6f6e29907372258aadb6b839d2
vitalii-motso <vitalii.motso@gmail.com>
2018-12-11 14:27:41 +0200

    use common utility to switch auction status (#24)

--- openprocurement/auctions/rubble/utils.py
+++ openprocurement/auctions/rubble/utils.py
@@ -4,10 +4,17 @@ from logging import getLogger
 from pkg_resources import get_distribution
 
 from openprocurement.auctions.core.utils import (
-    check_complaint_status, check_auction_status, remove_draft_bids,
-    upload_file as base_upload_file, get_file as base_get_file,
+    check_complaint_status,
+    check_auction_status,
+    remove_draft_bids,
+    upload_file as base_upload_file,
+    get_file as base_get_file,
     API_DOCUMENT_BLACKLISTED_FIELDS as DOCUMENT_BLACKLISTED_FIELDS,
-    context_unpack, calculate_business_date, get_now, TZ
+    context_unpack,
+    calculate_business_date,
+    get_now,
+    TZ,
+    log_auction_status_change
 )
 
 from .constants import (
@@ -74,22 +81,20 @@ def check_status(request):
         for complaint in award.complaints:
             check_complaint_status(request, complaint, now)
     if not auction.lots and auction.status == 'active.tendering' and auction.tenderPeriod.endDate <= now:
-        LOGGER.info('Switched auction {} to {}'.format(auction['id'], 'active.auction'),
-                    extra=context_unpack(request, {'MESSAGE_ID': 'switched_auction_active.auction'}))
         auction.status = 'active.auction'
         remove_draft_bids(request)
         remove_invalid_bids(request)
         check_bids(request)
-        return
+        log_auction_status_change(request, auction, auction.status)
+        return True
     elif auction.lots and auction.status == 'active.tendering' and auction.tenderPeriod.endDate <= now:
-        LOGGER.info('Switched auction {} to {}'.format(auction['id'], 'active.auction'),
-                    extra=context_unpack(request, {'MESSAGE_ID': 'switched_auction_active.auction'}))
         auction.status = 'active.auction'
         remove_draft_bids(request)
         remove_invalid_bids(request)
         check_bids(request)
         [setattr(i.auctionPeriod, 'startDate', None) for i in auction.lots if i.numberOfBids < 2 and i.auctionPeriod]
-        return
+        log_auction_status_change(request, auction, auction.status)
+        return True
     elif not auction.lots and auction.status == 'active.awarded':
         standStillEnds = [
             a.complaintPeriod.endDate.astimezone(TZ)
